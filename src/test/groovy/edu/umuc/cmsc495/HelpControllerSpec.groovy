dd3a052088aff08574a1329e760cfbea9b894d8f
Josh Hemen <melombuki@gmail.com>
2015-06-29 19:14:04 -0500

    Change ViewHelpController to HelpController in the HelpControllerSpec to match the renamed controller class rename.

--- src/test/groovy/edu/umuc/cmsc495/HelpControllerSpec.groovy
+++ src/test/groovy/edu/umuc/cmsc495/HelpControllerSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(ViewHelpController)
+@TestFor(HelpController)
 class ViewHelpControllerSpec extends Specification {
 
     def setup() {
